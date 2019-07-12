8af3bf66581a01f79922a30084d2b68e34c113fa
Victor Matheus de Moraes e Silva <victor.moraes@softdesign.com.br>
2019-07-12 18:14:41 -0300

    primeiras aulas

---
+++ src/app/rotas/rotas.js
@@ -0,0 +1,23 @@
+module.exports = app => {
+    
+    app.get('/', function(req, res) {
+        res.send(
+            `
+                <html>
+                    <head>
+                        <meta charset="utf-8">
+                    </head>
+                    <body>
+                        <h1>Casa do Código</h1>
+                    </body>
+                </html>
+            `
+        );
+    });
+    
+    app.get('/livros', function(req, res) {
+        res.marko(
+            require('../views/livros/lista/lista.marko')
+        );
+    });
+}
